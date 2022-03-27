26fef3741e7c40126ce0acc44ec1170d27e46b55
MneRon <daniil.bespolitov@yandex.ru>
2022-03-27 16:00:03 +0500

    add index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <style type="text/CSS (ЦСС)">
+        LI { list-style-type: none; } /* Убираем исходную нумерацию */
+        OL { counter-reset: list1; } /* Инициируем счетчик */
+        OL LI:before {
+            counter-increment: list1; /* Увеличиваем значение счетчика */
+            content: counter(list1) ". "; /* Выводим значение */
+        }
+        OL OL { counter-reset: list2; } /* Инициируем счетчик вложенного списка */
+        OL OL LI:before {
+            counter-increment: list2; /* Увеличиваем значение счетчика */
+            content: counter(list1) "." counter(list2) ". "; /* Выводим значение типа 2.1, 2.2,... */
+        }
+    </style>
+</head>
+<body>
+
+<header>Бесполитов Д.А.</header>
+
+<nav>Лабы</nav>
+<ol>
+    <li>Лаба 1</li>
+    <ol >
+        <li> <a>Задача 1</a> </li>
+        <l
+    </ol>
+    <li>Лаба 2</li>
+    <ol>
+        
+    </ol>
+    <li>Лаба 3</li>
+    <ol>
+        
+    </ol>
+    <li>Лаба 4</li>
+    <ol>
+        
+    </ol>
+	<li>Лаба 5</li>
+    <ol>
+        
+    </ol>
+</ol>
+
+
+
+
+
+
+</body>
+</html>
