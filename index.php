1ef82f609c5e0842bfd9d51609877921971830b2
MneRon <daniil.bespolitov@yandex.ru>
2022-03-27 17:02:24 +0500

    add Lab 3-7

--- index.php
+++ index.php
@@ -24,6 +24,11 @@
     <ol >
         <li> <a href= "Lab1.html" >Задание 1</a> </li>
         <li> <a href= "Lab2.html" >Задание 2</a> </li>
+        <li> <a href= "Lab3.html" >Задание 3</a> </li>
+        <li> <a href= "Lab4.html" >Задание 4</a> </li>
+        <li> <a href= "Lab5.html" >Задание 5</a> </li>
+        <li> <a href= "Lab6.html" >Задание 6</a> </li>
+        <li> <a href= "Lab6.html" >Задание 7</a> </li>
     </ol>
     <li>Лаба 2</li>
     <ol>
